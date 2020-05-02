c3ec80413a36bf46ded7cac43d41d47df64195e6
Evan McConnell <mcconnell.evan@gmail.com>
2020-05-02 23:21:00 +0100

    full NPC prefab

--- Assets/Scripts/AI/AIMovement.cs
+++ Assets/Scripts/AI/AIMovement.cs
@@ -8,17 +8,26 @@ public class AIMovement : MonoBehaviour
     [Header ("States")]
     public aiStates currentState;
     public enum aiStates { patrolling, chasing };
+    public bool alwaysChase;
+    public bool neverChase;
 
     [Header("NavMesh")]
     public NavMeshAgent agent;
     public float distanceFromTarget;
 
     [Header("Patrolling")]
-    public GameObject[] patrolPoints;
+    public GameObject[] points;
     public int nextPoint;
     public GameObject destination;
     public bool changeable = true;
     public int routeChangeBuffer;
+
+    [Header("Chaser")]
+    
+
+    [Header("Scanner")]
+    Ray playerFinder;
+    RaycastHit playerFound;
 
 
     // Start is called before the first frame update
@@ -26,24 +35,27 @@ public class AIMovement : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         currentState = aiStates.patrolling;
-        transform.position = patrolPoints[0].transform.position;
+        transform.position = points[0].transform.position;
         nextPoint = 1;
-        destination = patrolPoints[nextPoint];
+        destination = points[nextPoint];
     }
 
     // Update is called once per frame
     void Update()
     {
+        toggleStateBools();
+        scanForPlayer();
+
         distanceFromTarget = Vector3.Distance(destination.transform.position, transform.position);
         switch (currentState)
         {
             case aiStates.patrolling:
-                destination = patrolPoints[nextPoint];
+                destination = points[nextPoint];
                 if (distanceFromTarget > 2) { break; }
                 else { 
                     if (changeable == true) {
                         StartCoroutine(patrolBuffer(routeChangeBuffer));
-                        if (nextPoint == patrolPoints.Length - 1)
+                        if (nextPoint == points.Length - 1)
                         {
                             nextPoint = 0;
                         }
@@ -59,12 +71,32 @@ public class AIMovement : MonoBehaviour
         agent.SetDestination(destination.transform.position);
     }
 
+    void scanForPlayer()
+    {
+        playerFinder = new Ray(transform.position, agent.velocity);
+        Debug.DrawRay(transform.position, agent.velocity, Color.white, 10f);
+        /*playerFound = new RaycastHit();
+        Physics.Raycast(transform.position, agent.velocity, out playerFound);*/
+
+        
+        //if (playerFound.collider.gameObject.tag == "Player")
+        if (Physics.Raycast(transform.position, agent.velocity, out playerFound, 10f))
+        {
+            currentState = aiStates.chasing;
+        }
+    }
+
+    void toggleStateBools()
+    {
+        if (alwaysChase) { neverChase = false; }
+        if (neverChase) { alwaysChase = false; }
+    }
+
     IEnumerator patrolBuffer(int buffer)
     {
         changeable = false;
         yield return new WaitForSecondsRealtime(buffer);
         changeable = true;
-
     }
 }
 
