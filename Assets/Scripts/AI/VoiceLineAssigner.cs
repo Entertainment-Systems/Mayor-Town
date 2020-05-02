a0240e4bd882fb3b6d8974e888b6012a21937960
Kigaru <jakubster9@gmail.com>
2020-05-02 23:52:55 +0100

    Added VoiceLineAssigner

---
+++ Assets/Scripts/AI/VoiceLineAssigner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLineAssigner : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip[] voiceLines;
+    AudioSource audioSource;
+
+    IEnumerator playVoiceLine()
+    {
+        audioSource.clip = voiceLines[Random.Range(0, voiceLines.Length - 1)];
+        audioSource.Play();
+        yield return new WaitForSecondsRealtime(5);
+        StartCoroutine(playVoiceLine());
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(playVoiceLine());
+    }
+
+
+
+}
