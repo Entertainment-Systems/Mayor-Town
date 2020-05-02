9294581ec2f377e65bc20a6ade0a2c139d771697
Ciarán Malone <ciaranmalone27@gmail.com>
2020-05-02 12:31:45 +0100

    things I have done that are bad

---
+++ Assets/CollideWithPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideWithPlayer : MonoBehaviour
+{
+    public Transform resetPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("waiting begon");
+        collision.transform.position = resetPosition.position;
+      //  Time.timeScale = 0f;
+        StartCoroutine(Reset());
+    }
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(2);
+
+        print("waited");
+    }
+}
--- Assets/audioQuality.cs
+++ Assets/audioQuality.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class audioQuality : MonoBehaviour
 {
     private AudioSource audioS;
+    private AudioDistortionFilter audioDistortionFilter;
+
+    [Range(0.0f, 1.0f)]
+    public float audioDistortion;
 
     void Start()
     {
         audioS = GetComponent<AudioSource>();
+        audioDistortionFilter = GetComponent<AudioDistortionFilter>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    //    audioS.audioQuality
+        audioDistortionFilter.distortionLevel = audioDistortion;
     }
 }
