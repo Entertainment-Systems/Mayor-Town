9f86bc3de155c2fe20df23b921b4571617d944f4
Evan McConnell <mcconnell.evan@gmail.com>
2020-05-03 16:47:43 +0100

    pause menu activation

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool paused = false;
+    public GameObject pauseScreen;
+    public GameObject gameOverlay;
+    public GameObject scoreCounter;
+    public characterController cc;
+    public camMouseLook cml;
+
     void Start()
     {
-        
+        cc = GameObject.Find("Player").GetComponent<characterController>();
+        cml = cc.gameObject.transform.GetChild(0).GetComponent<camMouseLook>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreCounter.GetComponent<TextMeshProUGUI>().text = "(Score: " + PlayerPrefs.GetInt("collected") + ")";
+
+        if (Input.GetKeyDown(KeyCode.Escape)) { paused = !paused; }
+
+        pauseToggle();
     }
+
+    void pauseToggle()
+    {
+        if (paused)
+        {
+            Cursor.visible = true;
+            gameOverlay.SetActive(false);
+            cc.lockCursor = false;
+            //cc.enabled = false;
+            cml.enabled = false;
+            pauseScreen.SetActive(true);
+            //Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Cursor.visible = false;
+            gameOverlay.SetActive(true);
+            //cc.enabled = true;
+            cc.lockCursor = true;
+            cml.enabled = true;
+            pauseScreen.SetActive(false);
+        }
+    }
+
+    public void resume() { paused = false; }
 }
