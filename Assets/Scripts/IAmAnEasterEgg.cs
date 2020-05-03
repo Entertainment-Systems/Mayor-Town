f27416e6d4b7d3dfcf2cffe90cb0d3fabbc1fe7e
Kigaru <jakubster9@gmail.com>
2020-05-03 19:53:11 +0100

    AAA CURSOR IN ENDSCREEN WORKS FINALLY

---
+++ Assets/Scripts/IAmAnEasterEgg.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAmAnEasterEgg : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
