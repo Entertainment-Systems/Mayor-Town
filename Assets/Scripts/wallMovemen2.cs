fd63e8a0aa99346eb1a68afd1be92dc4d9cea5f3
Kevin Atkins <atkinsk021@gmail.com>
2020-05-03 16:19:40 +0100

    GAME OVER SCREEN

---
+++ Assets/Scripts/wallMovemen2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallMovemen2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+     float scrollSpeed = 0.1f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer> ();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, offset));
+        
+    }
+}
