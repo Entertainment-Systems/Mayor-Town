d2aef28eb32537012f0ff9a0e56ee9fb377da5bc
Evan McConnell <mcconnell.evan@gmail.com>
2020-05-03 19:26:34 +0100

    End Scene Done

---
+++ Assets/EndQuitter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndQuitter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(quit());
+    }
+
+    IEnumerator quit()
+    {
+        yield return new WaitForSecondsRealtime(42);
+        Application.Quit(0);
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
